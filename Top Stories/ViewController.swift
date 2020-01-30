99379023ac210efdd0a39dd902553a6357435adb
tommy colella <tcolella@bsd220.org>
2020-01-30 10:13:05 -0600

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Tommy Colella on 1/30/20.
+//  Copyright © 2020 Tommy Colella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
